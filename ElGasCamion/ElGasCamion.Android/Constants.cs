2f12085020cfd7475e718e3b5f9a1475fd96e8c5
Brian Molina <bmolina@digitalstrategy.com.ec>
2018-07-03 14:04:58 -0500

    Push notification

--- ElGasCamion/ElGasCamion.Android/Constants.cs
+++ ElGasCamion/ElGasCamion.Android/Constants.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ElGasCamion
+﻿
+namespace ElGasCamion.Droid
 {
     public class Constants
     {
-        public static string BaseApiAddress => "http://webapielgas.azurewebsites.net/";
-        #region DataGCM
         public const string SenderID = "5570742533"; // Google API Project Number
         public const string ListenConnectionString = "Endpoint=sb://notificacionesdesarrollo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=B10SdmICrxrk4RAZ3abyGBB3SdrntdmW+iKImvByLIQ=";
         public const string NotificationHubName = "notificacionesds";
-        #endregion
     }
+
 }
--- ElGasCamion/ElGasCamion.Android/MainActivity.cs
+++ ElGasCamion/ElGasCamion.Android/MainActivity.cs
@@ -8,12 +8,15 @@ using Android.Widget;
 using Android.OS;
 using TK.CustomMap.Droid;
 using Plugin.Permissions;
+using Android.Util;
 
 namespace ElGasCamion.Droid
 {
     [Activity(Label = "ElGasCamion", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public const string TAG = "MainActivity";
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -22,6 +25,19 @@ namespace ElGasCamion.Droid
           
 
             base.OnCreate(bundle);
+
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    if (key != null)
+                    {
+                        var value = Intent.Extras.GetString(key);
+                        Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                    }
+                }
+            }
+
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             Xamarin.FormsMaps.Init(this, bundle);
